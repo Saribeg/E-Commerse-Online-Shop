4594fb3d0cf0155f7cdc59955b50eed613570dd4
Denis Kanivets <42944484+DenisKanivets@users.noreply.github.com>
2019-02-02 15:48:52 +0200

    Carousel on main page (#4)
    * test carousel on main page
    
    * del .idea
    
    * delete

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProductsCarousel from './components/ProductsCarousel'
+
 
 class App extends Component {
   render() {
@@ -20,6 +22,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <ProductsCarousel/>
       </div>
     );
   }
