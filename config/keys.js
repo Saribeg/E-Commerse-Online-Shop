97ecf69a59333cea69bfc2c9316833d3f43cfb8d
Saribeg <37780211+Saribeg@users.noreply.github.com>
2019-02-02 13:51:16 +0200

    DB config and minimal info (#1)

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://ecommersefpdanit:m2018itDansvp2!@ds249757.mlab.com:49757/ecommersefpdanit"
+};
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  login: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
