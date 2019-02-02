8bae6970fd0b9424c7f51183f4b91d6961a5c9d0
IrinaStoetski <30476402+IrinaStoetski@users.noreply.github.com>
2019-02-02 15:34:29 +0200

    Gulpsass (#3)
    * temp folder
    
    * temp folder

--- temp/gulpfile.js
+++ temp/gulpfile.js
@@ -7,5 +7,5 @@ var sass = require('gulp-sass');
 gulp.task('sass', function(){
 	gulp.src('./scss/*.scss')
 			.pipe(sass())
-			.pipe(gulp.dest('./css/*.css'));
+			.pipe(gulp.dest('./css/'));
 });
