bb291cf54f0cf1ada75f22fcf9316cc6bfb55c3b
Saribeg <saribeg@gmail.com>
2019-02-09 15:37:50 +0200

    Navigation menu component with redux store, the first part

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
+import "./scss/style.scss";
 // import ProductsCarousel from './components/ProductsCarousel'
 // import TopBlockAuth from './components/TopBlockAuth'
 
@@ -7,9 +8,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainPage from "./components/MainPage";
 import Profile from "./components/Profile";
-
-// import "./scss/style.scss";
-import "./app.scss";
+// import ProductPage from "./components/ProductPage";
 
 class App extends Component {
   render() {
@@ -19,6 +18,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={MainPage} />
           <Route exact path="/profile" component={Profile} />
+          {/*<Route exact path="/product/1" component={ProductPage}/>*/}
         </Switch>
         <Footer />
       </Fragment>
--- routes/users.js
+++ routes/users.js
@@ -10,89 +10,76 @@ const validateRegisterInput = require("../validation/register");
 const validateLoginInput = require("../validation/login");
 
 router.get("/test", (req, res) => {
-    res.json({msg: "Users Works"});
+  res.json({ msg: "Users Works" });
 });
 
 // Add user to mongodb after server-side validations
 router.post("/register", (req, res) => {
+  // const {errors, isValid} = validateRegisterInput(req.body);
 
-    // const {errors, isValid} = validateRegisterInput(req.body);
+  // Check validation
+  // if (!isValid) {
+  //
+  //     console.log('hi errors');
+  //     return res.status(400).json(errors); //If not valid - send information about errors to client-side (React)
+  // }
 
-    // Check validation
-    // if (!isValid) {
-    //
-    //     console.log('hi errors');
-    //     return res.status(400).json(errors); //If not valid - send information about errors to client-side (React)
-    // }
+  // Check if the user already exist
+  User.findOne({ email: req.body.email }).then(user => {
+    console.log("hi");
+    if (user) {
+      // if user allready exist then deny request from front
+      res.json({ result: false });
+    } else {
+      // If not exist - create user object according to mongoose schema
+      const newUser = new User({
+        firstName: req.body.firstName,
+        secondName: req.body.secondName,
+        email: req.body.email,
+        password: req.body.password
+      });
 
-    // Check if the user already exist
-    User.findOne({email: req.body.email}).then(user => {
-
-        console.log('hi');
-        if (user) {
-            // if user allready exist then deny request from front
-            res.json({result: false});
-
-        } else {
-            // If not exist - create user object according to mongoose schema
-            const newUser = new User({
-                firstName: req.body.firstName,
-                secondName: req.body.secondName,
-                email: req.body.email,
-                password: req.body.password
-            });
-
-            newUser
-                .save()
-                .then(user => res.json(user))
-                .catch(err => console.log(err));
-        }
-    });
+      newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+    }
+  });
 });
 
 //Send post-request from Login form
 router.post("/login", (req, res) => {
-    // const {errors, isValid} = validateLoginInput(req.body); //If not valid - send information about errors to client-side (React)
+  // const {errors, isValid} = validateLoginInput(req.body); //If not valid - send information about errors to client-side (React)
 
-    // Check validation
-    // if (!isValid) {
-    //     return res.status(400).json(errors);
-    // }
+  // Check validation
+  // if (!isValid) {
+  //     return res.status(400).json(errors);
+  // }
 
-    // get elements from body of form
-    const email = req.body.email;
-    const password = req.body.password;
+  // get elements from body of form
+  const email = req.body.email;
+  const password = req.body.password;
 
-    // Find user by email in mongoose
-    User.findOne({email: email}, function(err, user) {
+  // Find user by email in mongoose
+  User.findOne({ email: email }, function(err, user) {
+    console.log(user);
 
-        console.log(user);
-
-        // if any mistakes of user was not found - send to FRONT object with status FALSE
-        // If front get FALSE - then add field "incorrect login or password"
-        if (err || !user) {
-            res.json({result: false});
+    // if any mistakes of user was not found - send to FRONT object with status FALSE
+    // If front get FALSE - then add field "incorrect login or password"
+    if (err || !user) {
+      res.json({ result: false });
+    } else {
+      // if we get exist email then check password with bcrypt
+      user.comparePassword(password, function(err, isMatch) {
+        if (!isMatch) {
+          res.json({ result: false });
         } else {
-            // if we get exist email then check password with bcrypt
-            user.comparePassword(password, function(err, isMatch) {
-                if (!isMatch) {
-                    res.json({result: false});
-                }
-                else {
-                    // if all good - send object with data about user
-                    res.json(user);
-                }
-            });
-
+          // if all good - send object with data about user
+          res.json(user);
         }
-
-
-    })
-
-
+      });
+    }
+  });
 });
-
-
-
 
 module.exports = router;
