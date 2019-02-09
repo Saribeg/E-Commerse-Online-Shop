2b83f1297dda507203408c88a479514586ad0d3e
Saribeg <37780211+Saribeg@users.noreply.github.com>
2019-02-09 16:02:06 +0200

    Fix merging (#33)
    * Products and Navogation Menu at backend
    
    * Carousel endpoints
    
    * Nav-menu component. Part 1
    
    * Navigation menu component with redux store, the first part

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,29 @@
-import React, {Component, Fragment} from 'react';
-import {Switch, Route} from 'react-router-dom';
-import './scss/style.scss';
+import React, { Component, Fragment } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./scss/style.scss";
 // import ProductsCarousel from './components/ProductsCarousel'
 // import TopBlockAuth from './components/TopBlockAuth'
 
-import Header from './components/Header'
-import Footer from './components/Footer'
-import MainPage from './components/MainPage'
-import Profile from './components/Profile'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainPage from "./components/MainPage";
+import Profile from "./components/Profile";
 // import ProductPage from "./components/ProductPage";
 
-
 class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={MainPage} />
-                    <Route path="/profile" component={Profile} />
-                    {/*<Route exact path="/product/1" component={ProductPage}/>*/}
-
-                </Switch>
-                <Footer />
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/profile" component={Profile} />
+          {/*<Route exact path="/product/1" component={ProductPage}/>*/}
+        </Switch>
+        <Footer />
+      </Fragment>
+    );
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
