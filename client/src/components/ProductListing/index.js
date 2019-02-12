08a8d00032fef0e368ceb16636ca4b3df56fac71
Saribeg <37780211+Saribeg@users.noreply.github.com>
2019-02-12 18:41:34 +0200

    Product info (#43)
    * Products and Navogation Menu at backend
    
    * Carousel endpoints
    
    * Nav-menu component. Part 1
    
    * Navigation menu component with redux store, the first part
    
    * Navigation Menu onMouseOver depends on overed category
    
    * New Product Structure
    
    * fixed db pruduct structure
    
    * New product photos, adding products to db, fixing product object structure etc.

--- client/src/components/ProductListing/index.js
+++ client/src/components/ProductListing/index.js
@@ -1,21 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 import ProductCard from "../ProductCard";
-import './ProductListing.scss'
+import "./ProductListing.scss";
 
-
-class ProductListing extends Component{
-    render() {
-        let listProduct = [];
-        for(let i=0; i<this.props.items; i++){
-            listProduct.push(<ProductCard />)
-        }
-        return(
-            <div className="listing-products">
-                {listProduct}
-            </div>
-
-        )
+class ProductListing extends Component {
+  render() {
+    let listProduct = [];
+    for (let i = 0; i < this.props.items; i++) {
+      listProduct.push(<ProductCard />);
     }
+    return <div className="listing-products">{listProduct}</div>;
+  }
 }
 
 export default ProductListing;
