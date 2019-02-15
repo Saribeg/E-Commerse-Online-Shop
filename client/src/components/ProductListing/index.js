d6b0b9e6999a887cd735679a35623cb3d4a31a34
Ann <annagorova66@gmail.com>
2019-02-15 23:12:39 +0200

    rename item

--- client/src/components/ProductListing/index.js
+++ client/src/components/ProductListing/index.js
@@ -11,16 +11,16 @@ class ProductListing extends Component {
     }
 
     render() {
-        let listProduct = this.props.productListing.map(item => {
+        let listProduct = this.props.productListing.map(productItem => {
 
-            return item.productFeatures.map(item2 => {
+            return productItem.productFeatures.map(item => {
                 return (
                     <>
-                        <NavLink key={item2._id} to={item.productUrl} className="product-item">
-                            {/*<img src={item2.imageUrls[0]} alt={item.model} className="product-img" />*/}
-                            <img src="../../img/featured.png" alt={item.model} className="product-img"/>
-                            <p className="product-name">{item.model}</p>
-                            <p className="product-price">${item.currentPrice}</p>
+                        <NavLink key={item._id} to={productItem.productUrl} className="product-item">
+                            {/*<img src={item.imageUrls[0]} alt={productItem.model} className="product-img" />*/}
+                            <img src="../../img/featured.png" alt={productItem.model} className="product-img"/>
+                            <p className="product-name">{productItem.model} ({item.colorName})</p>
+                            <p className="product-price">${productItem.currentPrice}</p>
                         </NavLink>
 
                     </>
