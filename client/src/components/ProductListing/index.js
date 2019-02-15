3b21b6d6c2c3e5a1de19a27316a864ca514542db
dmytrolevchuk <44846640+dmytrolevchuk@users.noreply.github.com>
2019-02-16 00:03:10 +0200

    move img of products to PUBLIC (#54)

--- client/src/components/ProductListing/index.js
+++ client/src/components/ProductListing/index.js
@@ -11,14 +11,21 @@ class ProductListing extends Component {
     }
 
     render() {
+
+        let counterItems = 0;
+
         let listProduct = this.props.productListing.map(productItem => {
 
             return productItem.productFeatures.map(item => {
+                counterItems++;
+
+                if (counterItems <= 8)
+
                 return (
                     <>
                         <NavLink key={item._id} to={productItem.productUrl} className="product-item">
-                            {/*<img src={item.imageUrls[0]} alt={productItem.model} className="product-img" />*/}
-                            <img src="../../img/featured.png" alt={productItem.model} className="product-img"/>
+                            <img src={item.imageUrls[0]} alt={productItem.model} className="product-img" />
+                            {/*<img src="../../img/featured.png" alt={productItem.model} className="product-img"/>*/}
                             <p className="product-name">{productItem.model} ({item.colorName})</p>
                             <p className="product-price">${productItem.currentPrice}</p>
                         </NavLink>
