bc7b81d68da0ab2b4a49aba31ad20121d93880a8
Denis Kanivets <42944484+DenisKanivets@users.noreply.github.com>
2019-02-17 13:50:05 +0200

    Updated carousel (#57)
    * subscribe email
    
    * style carousel, add subscribe mail to new collection subscribes

--- client/src/components/Subscribe/index.js
+++ client/src/components/Subscribe/index.js
@@ -1,37 +1,42 @@
-// import React, {Component} from 'react';
-// import './Subscribe.scss'
-// import axios from 'axios';
-//
-// class Subscribe extends Component {
-//
-//     subscribeMail = {};
-//
-//     sendMail = (e) => {
-//         e.preventDefault();
-//         axios.post('/subscribe', {subscribeMail})
-//             .then(res => {
-//                 console.log(res);
-//                 console.log(res.data);
-//             });
-//
-//     };
-//
-//     render() {
-//
-//         return (
-//             <>
-//                 <div className="subscribe-section">
-//                     <h4 className="subscribe-text">Subscribe</h4>
-//                     <p className="subscribe-info">Get the day’s top news stories<br/>delivered to your inbox</p>
-//                     <form onSubmit={this.sendMail} className="input-form">
-//                         <input id='subscribe-mail' className="input-email__text" type="email"
-//                                placeholder="Enter your email here..."/>
-//                         <input id='subscribe-btn' type="submit" className="subscribe-button" value='SUBSCRIBE'/>
-//                     </form>
-//                 </div>
-//             </>
-//         )
-//     }
-// }
-//
-// export default Subscribe;
+import React, {Component} from 'react';
+import './Subscribe.scss'
+import axios from 'axios';
+
+class Subscribe extends Component {
+
+    state = {
+        mail: ''
+    };
+
+    handleChange = (e) => {
+        this.setState({mail: e.target.value})
+    };
+
+    sendMail = (e) => {
+        e.preventDefault();
+        const subMail = {
+            mail: this.state.mail
+        };
+        axios.post('/subscribe', {subMail});
+        console.log(subMail.mail)
+    };
+
+    render() {
+
+        return (
+            <>
+                <div className="subscribe-section">
+                    <h4 className="subscribe-text">Subscribe</h4>
+                    <p className="subscribe-info">Get the day’s top news stories<br/>delivered to your inbox</p>
+                    <form onSubmit={this.sendMail} className="input-form">
+                        <input onChange={this.handleChange} id='subscribe-mail' className="input-email__text" type="email"
+                               placeholder="Enter your email here..." value={this.state.mail}/>
+                        <input id='subscribe-btn' type="submit" className="subscribe-button" value='SUBSCRIBE'/>
+                    </form>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Subscribe;
