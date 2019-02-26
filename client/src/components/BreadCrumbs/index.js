758ba061f764009265011d7618cd205c98618c46
Ann <annagorova66@gmail.com>
2019-02-26 21:11:03 +0200

    created breadCrumbs component

--- client/src/components/BreadCrumbs/index.js
+++ client/src/components/BreadCrumbs/index.js
@@ -1,13 +1,46 @@
-import React, { Component, NavLink} from 'react'
+import React, {Component} from "react";
+import {NavLink} from "react-router-dom";
 
-export default class ProductCard extends Component {
-	render() {
-		let paths = [];
-		return (
-			<ul className="breadcrumbs">
-       
-     </ul>
 
-		)
-	}
+import "./BreadCrumbs.scss";
+
+class BreadCrumbs extends Component {
+
+    componentDidMount() {
+        console.log("=========");
+    }
+
+    render() {
+
+        // let a = this.props.navMenuItems.map(item =>{
+        //     return (
+        //         <li><NavLink to="/">{item.categoryName}</NavLink></li>
+        //
+        //     )
+        // })
+
+        // let paths = [];
+        return (
+            <section className="breadcrumbs-block">
+                <div className="container">
+                    <div className="breadcrumbs">
+                        <div className="breadcrumbs-row">
+                            <ul>
+                                {/*{a}*/}
+                                <li><NavLink to="/">home</NavLink></li>
+                                <li><NavLink to="/">women</NavLink></li>
+                                <li><NavLink to="/">women's clothing</NavLink></li>
+                            </ul>
+                        </div>
+
+                        <div className="category-title-active">
+                            <h2>Women's clothing</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
+
+export default BreadCrumbs;
--- client/src/components/FilteredProductList/index.js
+++ client/src/components/FilteredProductList/index.js
@@ -1,46 +1,52 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { NavLink } from "react-router-dom";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {NavLink} from "react-router-dom";
 
 import Filters from "./Filters";
+import ProductListing from "../ProductListing";
+import BreadCrumbs from "../BreadCrumbs";
 
 import "./filteredProductList.scss";
 
 class FilteredProductList extends Component {
-  render() {
-    return (
-      <section class="category-block">
-        <div class="container">
-          <div class="category-content">
-            <Filters />
+    render() {
+        return (
+            <>
+                <BreadCrumbs/>
+                <section className="category-block">
+                    <div className="container">
+                        <div className="category-content">
+                            <Filters/>
 
-            <div class="category-product-listing">
-              <div class="listing-products">
-                <NavLink to="/" class="product-item">
-                  <img src="../img/featured.png" alt="" class="product-img" />
-                  <p class="product-name">No-Iron Easy Care Sleeveless Shirt</p>
-                  <p class="product-price">$599.00</p>
-                </NavLink>
-                <NavLink to="/" class="product-item">
-                  <img src="../img/featured.png" alt="" class="product-img" />
-                  <p class="product-name">No-Iron Easy Care Sleeveless Shirt</p>
-                  <p class="product-price">$599.00</p>
-                </NavLink>
-                <NavLink to="/" class="product-item">
-                  <img src="../img/featured.png" alt="" class="product-img" />
-                  <p class="product-name">No-Iron Easy Care Sleeveless Shirt</p>
-                  <p class="product-price">$599.00</p>
-                </NavLink>
-              </div>
-              <div class="btn-loading-products">
-                <a to="/">loading</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
+                            <div className="category-product-listing">
+                                <ProductListing children={9}/>
+                                {/*<div className="listing-products">*/}
+                                {/*<NavLink to="/" className="product-item">*/}
+                                {/*<img src="../img/featured.png" alt="" className="product-img"/>*/}
+                                {/*<p className="product-name">No-Iron Easy Care Sleeveless Shirt</p>*/}
+                                {/*<p className="product-price">$599.00</p>*/}
+                                {/*</NavLink>*/}
+                                {/*<NavLink to="/" class="product-item">*/}
+                                {/*<img src="../img/featured.png" alt="" className="product-img"/>*/}
+                                {/*<p className="product-name">No-Iron Easy Care Sleeveless Shirt</p>*/}
+                                {/*<p className="product-price">$599.00</p>*/}
+                                {/*</NavLink>*/}
+                                {/*<NavLink to="/" class="product-item">*/}
+                                {/*<img src="../img/featured.png" alt="" class="product-img"/>*/}
+                                {/*<p class="product-name">No-Iron Easy Care Sleeveless Shirt</p>*/}
+                                {/*<p class="product-price">$599.00</p>*/}
+                                {/*</NavLink>*/}
+                                {/*</div>*/}
+                                <div className="btn-loading-products">
+                                    <NavLink to="/">loading</NavLink>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </>
+        );
+    }
 }
 
 export default connect()(FilteredProductList);
