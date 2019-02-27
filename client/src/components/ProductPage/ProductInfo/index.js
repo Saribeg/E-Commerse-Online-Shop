ead011a13fa2fdb2bd0e88b886d60a9ecac03e49
Irina <irinastoetski13@gmail.com>
2019-02-27 16:22:35 +0200

    product sizes

--- client/src/components/ProductPage/ProductInfo/index.js
+++ client/src/components/ProductPage/ProductInfo/index.js
@@ -1,29 +1,40 @@
 // @flow
-import * as React from 'react';
-import ProductColors from './ProductColors';
+import * as React from "react";
+import ProductColors from "./ProductColors";
+import ProductSizes from "./ProductSizes";
 
-class ProductInfo extends Component {
 type Props = {
-	currentPrice: number,
-  itemNo?: string,
+  currentPrice: number,
+  itemNo?: string
 };
 
 class ProductInfo extends React.Component<Props> {
-	render() {
-		const {itemNo, currentPrice, model, colors, activeColor} = {...this.props};
+  render() {
+    const { itemNo, currentPrice, model, productFeatures, activeColor } = {
+      ...this.props
+    };
+    const activeItem = productFeatures.filter(elem => {
+      return activeColor === elem.colorName;
+    })[0];
 
-		return (
-	<div className="product-main-info">
-			<h2 className="product-title">{model}</h2>
-			<span className="product-meta">Item No. {itemNo}</span>
-			<p className="product-price">{currentPrice}</p>
- 			<p className="product-filter"></p>
-			<ProductColors colors={colors} activeColor={activeColor} changeColor={this.props.changeColor}/>
-</div>
-	)
-
-	}
+    return (
+      <div className="product-main-info">
+        <h2 className="product-title">{model}</h2>
+        <span className="product-meta">Item No. {itemNo}</span>
+        <p className="product-price">{currentPrice}</p>
+        <p className="product-filter" />
+        <ProductColors
+          colors={productFeatures}
+          activeColor={activeColor}
+          changeColor={this.props.changeColor}
+        />
+        <ProductSizes
+          activeColor={activeColor}
+          productFeatures={productFeatures}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default ProductInfo;
