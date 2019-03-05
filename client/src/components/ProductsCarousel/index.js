5a4a965d48c033802105a66f93fed5acda765e70
IrinaStoetski <30476402+IrinaStoetski@users.noreply.github.com>
2019-03-05 20:49:48 +0200

    refactoring (#100)

--- client/src/components/ProductsCarousel/index.js
+++ client/src/components/ProductsCarousel/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from "react-responsive-carousel";
-import "./ProductsCarousel.scss";
+import "./products-carousel.scss";
 import {NavLink} from "react-router-dom";
 import axios from 'axios';
 
