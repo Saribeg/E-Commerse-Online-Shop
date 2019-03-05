20fc87892c0849cbb8c855ddfd84f94441f125f8
Saribeg <saribeg@gmail.com>
2019-03-05 16:27:07 +0200

    ForTests

--- client/src/reducers/productDetails/index.js
+++ client/src/reducers/productDetails/index.js
@@ -1,34 +1,29 @@
 import {
-	FETCH_PRODUCT_DETAIL_REQUEST, 
-	FETCH_PRODUCT_DETAIL_SUCCEED,
-	FETCH_PRODUCT_DETAIL_FAIL
+  FETCH_PRODUCT_DETAIL_REQUEST,
+  FETCH_PRODUCT_DETAIL_SUCCEED,
+  FETCH_PRODUCT_DETAIL_FAIL
 } from "../../actions/productDetails";
 
 const initialState = {
-	productOpened:  {
-		productFeatures: [
-			{ colorName: '',
-				imageUrls: [],
-				sizes: [{}]		
-			}
-		] 
-	}	
+  productOpened: {
+    productFeatures: [{ colorName: "", imageUrls: [], sizes: [{}] }]
+  }
 };
 
 function productDetails(state = initialState, action) {
-	switch (action.type) {
-			case FETCH_PRODUCT_DETAIL_REQUEST:
-					return {
-							...state
-					}
-			case FETCH_PRODUCT_DETAIL_SUCCEED:
-					return {
-							...state,
-						 productOpened: action.payload
-					}
-			default:
-					return{...state}
-	}
+  switch (action.type) {
+    case FETCH_PRODUCT_DETAIL_REQUEST:
+      return {
+        ...state
+      };
+    case FETCH_PRODUCT_DETAIL_SUCCEED:
+      return {
+        ...state,
+        productOpened: action.payload
+      };
+    default:
+      return { ...state };
+  }
 }
 
 export default productDetails;
