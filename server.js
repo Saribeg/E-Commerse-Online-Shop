c6b070f9cdf92801c9e2690bc5e2aa39354d6853
Kirill Smirnov <kir.smirnoff@gmail.com>
2019-03-10 16:42:43 +0200

    Async test example from your API

--- server.js
+++ server.js
@@ -73,3 +73,4 @@ app.use("/", categoryCarousels);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+module.exports = app; // для тестирования
--- unittest/categoryCarousels.test.js
+++ unittest/categoryCarousels.test.js
@@ -3,6 +3,7 @@ let chaiHttp = require('chai-http');
 const expect = chai.expect;
 
 // let cattest = require('../routes/categoryCarousels');
+let server = require('../server');
 
 chai.use(chaiHttp);
 
@@ -10,6 +11,17 @@ describe('/GET ', () => {
         it("/GET", (done) => {
             chai.request('https://swapi.co/api')
                     .get('/')
+                    .end(function (err, res) {
+                        expect(err).to.be.null;
+                        expect(res).to.have.status(200);
+                        done(err);
+                    })
+        });
+
+
+        it("/GET your route", (done) => {
+            chai.request(server)
+                    .get('/categoryCarousels')
                     .end(function (err, res) {
                         expect(err).to.be.null;
                         expect(res).to.have.status(200);
