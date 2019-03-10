21e0bf011f2282876f56a6a7ee7a188ce034ff80
Kirill Smirnov <kir.smirnoff@gmail.com>
2019-03-10 15:38:48 +0200

    Async test example

--- unittest/categoryCarousels.test.js
+++ unittest/categoryCarousels.test.js
@@ -1,36 +1,20 @@
-const request = require('supertest');
-const express = require('express');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let should = chai.should();
 const expect = chai.expect;
 
-let cattest = require('../routes/categoryCarousels');
+// let cattest = require('../routes/categoryCarousels');
 
 chai.use(chaiHttp);
 
 describe('/GET ', () => {
-        // it('it should GET all the books', async (done) => {
-        //     await chai.request(cattest)
-        //         .get('/categoryCarousels')
-        //         .end((err, res) => {
-        //             // expect(res).to.have.status(200);
-        //             res.should.have.status(200);
-        //             res.body.should.be.a('array');
-        //             done();
-        //         })
-        // })
-
-        it("/GET", async (done) => {
-            await request(cattest)
-                .get("/categoryCarousels")
-                .set('Accept', 'application/json')
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end((err) => {
-                    if (err) return done(err);
-                    done();
-                });
+        it("/GET", (done) => {
+            chai.request('https://swapi.co/api')
+                    .get('/')
+                    .end(function (err, res) {
+                        expect(err).to.be.null;
+                        expect(res).to.have.status(200);
+                        done(err);
+                    })
         });
     }
 );
