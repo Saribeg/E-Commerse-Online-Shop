2e7eba5b0e4cfb2f5d0057cfd8cef74bf541e4e3
Saribeg <saribeg@gmail.com>
2019-03-11 22:26:54 +0200

    Highlight when search

--- client/src/components/SearchResultItem/index.js
+++ client/src/components/SearchResultItem/index.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
+import AutosuggestHighlightMatch from "autosuggest-highlight/match";
+import AutosuggestHighlightParse from "autosuggest-highlight/parse";
+
 import "./searchResultItem.scss";
 
 class SearchResultItem extends Component {
   render() {
-    const { products } = this.props;
+    const { products, searchString } = this.props;
 
     let searchedProductList = products.map(product => {
       let productColors = product.productFeatures.map(color => {
@@ -32,6 +35,99 @@ class SearchResultItem extends Component {
 
       let uniqueProductSizes = [...new Set(productSizes)];
 
+      // Rendering category matches
+      let categoryMatches = AutosuggestHighlightMatch(
+        product.category,
+        searchString
+      );
+      let categoryParts = AutosuggestHighlightParse(
+        product.category,
+        categoryMatches
+      );
+
+      let categoryMatched = categoryParts.map((part, index) => {
+        const className = part.highlight ? "match" : null;
+        return <span className={className}>{part.text}</span>;
+      });
+
+      // Rendering sub-category matches
+      let subCategoryMatches = AutosuggestHighlightMatch(
+        product.subCategory,
+        searchString
+      );
+      let subCategoryParts = AutosuggestHighlightParse(
+        product.subCategory,
+        subCategoryMatches
+      );
+
+      let subCategoryMatched = subCategoryParts.map((part, index) => {
+        const className = part.highlight ? "match" : null;
+        return <span className={className}>{part.text}</span>;
+      });
+
+      // Rendering further sub-category matches
+      let furtherSubCategoryMatches = AutosuggestHighlightMatch(
+        product.furtherSubCategory,
+        searchString
+      );
+      let furtherSubCategoryParts = AutosuggestHighlightParse(
+        product.furtherSubCategory,
+        furtherSubCategoryMatches
+      );
+
+      let furtherSubCategoryMatched = furtherSubCategoryParts.map(
+        (part, index) => {
+          const className = part.highlight ? "match" : "unmatched-text";
+          return <span className={className}>{part.text}</span>;
+        }
+      );
+
+      // Rendering model name matches
+      let model = product.model
+        .split(" ")
+        .map(word => {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join(" ");
+
+      let modelMatches = AutosuggestHighlightMatch(model, searchString);
+      let modelParts = AutosuggestHighlightParse(model, modelMatches);
+
+      let modelMatched = modelParts.map((part, index) => {
+        const className = part.highlight ? "match" : "unmatched-text";
+        return <span className={className}>{part.text}</span>;
+      });
+
+      // Rendering itemNo matches
+      let itemNoMatches = AutosuggestHighlightMatch(
+        product.itemNo,
+        searchString
+      );
+      let itemNoParts = AutosuggestHighlightParse(
+        product.itemNo,
+        itemNoMatches
+      );
+
+      let itemNoMatched = itemNoParts.map((part, index) => {
+        const className = part.highlight ? "match" : "unmatched-text";
+        return <span className={className}>{part.text}</span>;
+      });
+
+      // Rendering size matches
+      let sizeMatches = AutosuggestHighlightMatch(
+        uniqueProductSizes.join(", "),
+        searchString
+      );
+      let sizeParts = AutosuggestHighlightParse(
+        uniqueProductSizes.join(", "),
+        sizeMatches
+      );
+
+      let sizeMatched = sizeParts.map((part, index) => {
+        const className = part.highlight ? "match" : "unmatched-text";
+        return <span className={className}>{part.text}</span>;
+      });
+
       return (
         <li className="search-result-item" key={product._id}>
           <Link to={product.productUrl} className="search-result-link">
@@ -48,15 +144,16 @@ class SearchResultItem extends Component {
                   <span className="search-result-item-subheading">
                     Categories:
                   </span>{" "}
-                  {`${product.category} / ${product.subCategory} / ${
-                    product.furtherSubCategory
-                  }`}
+                  <span>
+                    {categoryMatched} / {subCategoryMatched} / &nbsp;
+                    {furtherSubCategoryMatched}
+                  </span>
                 </span>
                 <span className="search-result-item-model">
                   <span className="search-result-item-subheading">
                     Model name:{" "}
                   </span>
-                  {product.model}
+                  {modelMatched}
                 </span>
                 <div className="search-result-item-colors">
                   <span className="search-result-item-subheading">Colors:</span>
@@ -67,7 +164,7 @@ class SearchResultItem extends Component {
                 <span className="search-result-item-sizes">
                   <span className="search-result-item-subheading">Sizes: </span>{" "}
                   <span className="search-result-item-sizes-values">
-                    {uniqueProductSizes.join(", ")}
+                    {sizeMatched}
                   </span>
                 </span>
               </div>
@@ -77,7 +174,7 @@ class SearchResultItem extends Component {
                     Product ID:{" "}
                   </span>{" "}
                   <span className="search-result-item-id-value">
-                    {product.itemNo}
+                    {itemNoMatched}
                   </span>
                 </span>
                 <span className="search-result-item-subheading">Price: </span>
@@ -104,7 +201,8 @@ class SearchResultItem extends Component {
 
 const mapStateToProps = state => {
   return {
-    products: state.search.products
+    products: state.search.products,
+    searchString: state.search.searchString
   };
 };
 
