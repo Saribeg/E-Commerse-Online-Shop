32764bcdeaef84988bab8ada6e5a2dafa92159e7
annbezpala <44921007+annbezpala@users.noreply.github.com>
2019-03-16 11:12:54 +0200

    Bug fix (#125)
    * save
    
    * components - card product and product listing
    
    * add component product-page and add route
    
    * delete component product-page
    
    * update scss
    
    * start page category
    
    * finish page category(filter without adaptive)
    
    * start action and reducer for product
    
    * date for product to redux store
    
    * rename item
    
    * clear code
    
    * create model for categoriesList
    
    * create routes - post, get
    
    * create action, reducer, component for categoryCarousel
    
    * add photo for category
    
    * add photo(hats) and update style for category
    
    * created breadCrumbs component
    
    * save breadcrumbs
    
    * breadcrumbs update
    
    * finished breadcrumbs
    
    * start test for category
    
    * created folder for tests
    
    * save
    
    * test for category not work
    
    * update breadcrumbs
    
    * added products
    
    * added products - belts,wallets
    
    * working test for categorycarousel
    
    * save tests
    
    * finished tests for categoryCarousels
    
    * update tests for routers -carousels, menu, products
    
    * test for footer-links
    
    * tests for staticPages
    
    * update tests
    
    * save test for categotyItems
    
    * started fix bugs

--- client/src/components/ProductListing/index.js
+++ client/src/components/ProductListing/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {getProductItem, saveProductDetails} from "../../actions/product";
+import {getProductItem} from "../../actions/product";
 import {NavLink} from "react-router-dom";
 import "./ProductListing.scss";
 
@@ -11,25 +11,18 @@ class ProductListing extends Component {
     }
 
     render() {
-
         let counterItems = 0;
-
         let listProduct = this.props.productListing.map(productItem => {
-
             return productItem.productFeatures.map(item => {
                 counterItems++;
 
                 if (counterItems <= this.props.children)
                 return (
-                   
                         <NavLink key={item._id} to={productItem.productUrl} className="product-item">
-                       
                             <img src={item.imageUrls[0]} alt={productItem.model} className="product-img" />
                             <p className="product-name">{productItem.model} ({item.colorName})</p>
                             <p className="product-price">${productItem.currentPrice}</p>
-                      
                         </NavLink>
-                   
                 )
             })
         })
