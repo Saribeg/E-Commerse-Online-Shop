666754082b37c3f3c97f5dd6a5aea28f9a9b5e78
Irina <irinastoetski13@gmail.com>
2019-03-28 20:25:24 +0200

    build

---
+++ client/build/precache-manifest.a14bae0efc0387c4ee003aacb8f24458.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a617dfb6304f7cd3ec2341bda2f99505",
+    "url": "/static/media/user-solid.a617dfb6.svg"
+  },
+  {
+    "revision": "5ee19603be0e7d6af527cb982cecebad",
+    "url": "/static/media/matter.5ee19603.svg"
+  },
+  {
+    "revision": "0f8c649234a975a89330155dc9451460",
+    "url": "/static/media/matter-white.0f8c6492.svg"
+  },
+  {
+    "revision": "09c551e4884e1d6bf1e12e2c502592c4",
+    "url": "/static/media/featured.09c551e4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01886790f2c71b3da9c2",
+    "url": "/static/js/main.01886790.chunk.js"
+  },
+  {
+    "revision": "f067ae417d62598e4387",
+    "url": "/static/js/1.f067ae41.chunk.js"
+  },
+  {
+    "revision": "01886790f2c71b3da9c2",
+    "url": "/static/css/main.55044a1d.chunk.css"
+  },
+  {
+    "revision": "f067ae417d62598e4387",
+    "url": "/static/css/1.199f8018.chunk.css"
+  },
+  {
+    "revision": "011be15d33deb8e890772ea137f6286a",
+    "url": "/index.html"
+  }
+];
