9b9b4a0d55f2b4f551cf2cee530f438f6b3d8a55
Saribeg <saribeg@gmail.com>
2019-03-29 19:29:09 +0200

    Fixes merging

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
 
