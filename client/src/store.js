1bf5bc7c81e276a612d378621240779237b0410d
Saribeg <37780211+Saribeg@users.noreply.github.com>
2019-03-29 15:55:43 +0200

    Improvements (#163)
    * Products and Navogation Menu at backend
    
    * Carousel endpoints
    
    * Nav-menu component. Part 1
    
    * Navigation menu component with redux store, the first part
    
    * Navigation Menu onMouseOver depends on overed category
    
    * New Product Structure
    
    * fixed db pruduct structure
    
    * New product photos, adding products to db, fixing product object structure etc.
    
    * Fixing of bags from browser console and some comments on code for team
    
    * Header styling and bag-fixing
    
    * Styling bag fix
    
    * Admin Dashboard initial commit
    
    * Fix Adm
    
    * AdmRoutes
    
    * Admin Dashboard for nav menu part 1
    
    * NavMenu Admin Dashboard with full functionality
    
    * Components FilteredProductList and Filters without functionality of filtration
    
    * Back-end for filter items
    
    * Filters. Part1
    
    * ProductCard
    
    * FiltersFix
    
    * Filters bagfix and priceFilters
    
    * FilterColors
    
    * Filters are ready including NavMenu and Empty State
    
    * ForTests
    
    * Renamed files
    
    * SearchComponentsAndRoute
    
    * Live search
    
    * Highlight when search
    
    * SearchClose
    
    * Admin panel for adding new products to server
    
    * Improving new product saving, updating color and size information
    
    * Prefix
    
    * Product quantity added
    
    * Administration of Colors
    
    * Administration of sizes
    
    * Color circles style fix
    
    * FixColorAndSizeAdmin
    
    * Deploy preparation

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,8 @@
-import {createStore, applyMiddleware, compose} from 'redux'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers/index'
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = process.env.NODE_ENV === 'development' ? 
-createStore(rootReducer, composeEnhancers(applyMiddleware(thunk,logger)))
-: createStore(rootReducer, applyMiddleware(thunk));
-
-
-
-
-
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
