ce730c6f89f1f2c8c356f63b6a77eb0eda0b25e5
Saribeg <saribeg@gmail.com>
2019-04-02 16:14:22 +0300

    Build

---
+++ client/build/precache-manifest.4013f7c783bab9891ddf131372531416.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a617dfb6304f7cd3ec2341bda2f99505",
+    "url": "/static/media/user-solid.a617dfb6.svg"
+  },
+  {
+    "revision": "c6c8e62735c40b6c3287669ad9365c63",
+    "url": "/static/media/matter.c6c8e627.svg"
+  },
+  {
+    "revision": "87e92378af3b7afc5659a02b3833614e",
+    "url": "/static/media/matter-white.87e92378.svg"
+  },
+  {
+    "revision": "09c551e4884e1d6bf1e12e2c502592c4",
+    "url": "/static/media/featured.09c551e4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ddf89cfb6daef2ff582",
+    "url": "/static/js/main.5ddf89cf.chunk.js"
+  },
+  {
+    "revision": "1f1a26f284ca556d42dc",
+    "url": "/static/js/1.1f1a26f2.chunk.js"
+  },
+  {
+    "revision": "5ddf89cfb6daef2ff582",
+    "url": "/static/css/main.2b85ec45.chunk.css"
+  },
+  {
+    "revision": "1f1a26f284ca556d42dc",
+    "url": "/static/css/1.670625ee.chunk.css"
+  },
+  {
+    "revision": "c72c1dcefd90d67f67937a0ba44d9589",
+    "url": "/index.html"
+  }
+];
