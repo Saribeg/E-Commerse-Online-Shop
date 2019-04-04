52f207d765f7717b9d90589863bbd31b1a0a083a
dmytrolevchuk <44846640+dmytrolevchuk@users.noreply.github.com>
2019-04-04 22:32:28 +0300

    changes in product page (#187)

---
+++ client/build/precache-manifest.fb107d858f317623c5e7e66d7b40e689.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a617dfb6304f7cd3ec2341bda2f99505",
+    "url": "/static/media/user-solid.a617dfb6.svg"
+  },
+  {
+    "revision": "5ee19603be0e7d6af527cb982cecebad",
+    "url": "/static/media/matter.5ee19603.svg"
+  },
+  {
+    "revision": "0f8c649234a975a89330155dc9451460",
+    "url": "/static/media/matter-white.0f8c6492.svg"
+  },
+  {
+    "revision": "09c551e4884e1d6bf1e12e2c502592c4",
+    "url": "/static/media/featured.09c551e4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c419ebe2f131bd9b82a9",
+    "url": "/static/js/main.c419ebe2.chunk.js"
+  },
+  {
+    "revision": "8c183e262766842c53a4",
+    "url": "/static/js/1.8c183e26.chunk.js"
+  },
+  {
+    "revision": "c419ebe2f131bd9b82a9",
+    "url": "/static/css/main.26b176ec.chunk.css"
+  },
+  {
+    "revision": "8c183e262766842c53a4",
+    "url": "/static/css/1.142a143c.chunk.css"
+  },
+  {
+    "revision": "e9a8f33aaa88a897ffc72c4e9aa460c0",
+    "url": "/index.html"
+  }
+];
